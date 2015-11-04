441a22e454ac04828f083ae6594bd03aee2df6bb
castamir <miras.paulik@email.cz>
2015-11-04 11:58:49 +0100

    Query: improved PhpDoc

--- src/Joseki/LeanMapper/Query.php
+++ src/Joseki/LeanMapper/Query.php
@@ -3,10 +3,10 @@
 namespace Joseki\LeanMapper;
 
 /**
- * @method Query where($condition, $_)
- * @method Query orderBy($field, $_)
- * @method Query asc(bool $asc = TRUE, $_)
- * @method Query desc(bool $desc = TRUE, $_)
+ * @method Query where($condition, $_ = null)
+ * @method Query orderBy($field, $_ = null)
+ * @method Query asc(bool $asc = TRUE, $_ = null)
+ * @method Query desc(bool $desc = TRUE, $_ = null)
  */
 class Query extends \LeanMapperQuery\Query
 {
